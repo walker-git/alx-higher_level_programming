744e2b431bb69faedeeab85efe76a555225faade
Etikerentse Samuel <setiks92@gmail.com>
2022-12-20 17:29:35 -0800

    task 5

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
-def raise_exception():
-    """Raise a TypeError exception."""
-
-raise TypeError
-
-
+def raise_exception_msg(message=""):
+    """Raise a NameError exception with a message."""
+    raise NameError(message)
