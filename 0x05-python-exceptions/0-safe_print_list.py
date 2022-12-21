519fc46210fcef0b09f6a294cca5c9ec5e7ff7f0
Etikerentse Samuel <setiks92@gmail.com>
2022-12-20 16:27:22 -0800

    A function that prints X elements of a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,21 +1,37 @@
 #!/usr/bin/python3
 
+
+
 def safe_print_list(my_list=[], x=0):
 
-    cmpt = 0
+    """Print x elememts of a list.
 
-    for index in range(x):
+    Args:
+
+        my_list (list): The list to print elements from.
+
+        x (int): The number of elements of my_list to print.
+
+    Returns:
+
+        The number of elements printed.
+
+    """
+
+    ret = 0
+
+    for i in range(x):
 
         try:
 
-            print(my_list[index], end='')
+            print("{}".format(my_list[i]), end="")
 
-            cmpt += 1
+            ret += 1
 
-        except Exception as error:
+        except IndexError:
 
             break
 
-    print('')
+    print("")
 
-    return 
+    return (ret)
