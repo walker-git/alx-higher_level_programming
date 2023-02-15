cc870b7aa371445454a96221b99a55a501ad907d
Etikerentse Samuel <setiks92@gmail.com>
2023-02-15 03:53:30 -0800

    A script that creates a table in the current database

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- Write a script that creates a table called first_table in the current database in MySQL server.
+CREATE TABLE first_table (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  age INT NOT NULL,
+  PRIMARY KEY (id)
+);
