2e07a2bd0d093529e379b29035403c0faf12412a
Etikerentse Samuel <setiks92@gmail.com>
2023-02-15 04:08:20 -0800

    A script that creates a table in the database called first_table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,7 +1,2 @@
--- Write a script that creates a table called first_table in the current database in MySQL server.
-CREATE TABLE first_table (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(255) NOT NULL,
-  age INT NOT NULL,
-  PRIMARY KEY (id)
-);
+-- Creates a table called first_table with values id and name in my MySQL server.
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
